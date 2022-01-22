0d505e5bd3d07f9f6aa5b3dd1dfde60c83d95592
email/username: sukhdev.singh@smartdatainc.net/sukhdevs <sukhdev.singh@smartdatainc.net>
2022-01-22 13:30:52 +0530

    added logo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://oradfoundation.azurewebsites.net/api/v1',
- // baseUrl:'https://oradfoundation.azurewebsites.net',
-  baseUrl:'http://3.138.155.186:5006',
-  apiUrl: 'http://3.138.155.186:5006/api/v1'  
+  apiUrl: 'https://oradfoundation.azurewebsites.net/api/v1',
+   baseUrl:'https://oradfoundation.azurewebsites.net'
+  // baseUrl:'http://3.138.155.186:5006',
+  // apiUrl: 'http://3.138.155.186:5006/api/v1'  
 };
 
